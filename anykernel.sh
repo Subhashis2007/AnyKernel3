6b52388125a323f2be9182524629f991230df491
s-rs <baradsubhashis@gmail.com>
2023-09-10 07:23:53 +0530

    adapt for spartan

--- anykernel.sh
+++ anykernel.sh
@@ -4,17 +4,17 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=Smooth
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
-device.name5=
+device.name1=RMX1821
+device.name2=RMX1822
+device.name3=RMX1823
+device.name4=RMX1825
+device.name5=RMX1827
 supported.versions=
 supported.patchlevels=
 supported.vendorpatchlevels=
@@ -29,7 +29,7 @@ set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 } # end attributes
 
 # boot shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
+block=/dev/block/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 patch_vbmeta_flag=auto;
