dbc5401e44967dbc0a17fa37c81831b39e2df7f5
Subhashis <113865986+Subhashis2007@users.noreply.github.com>
2023-10-18 09:25:41 +0530

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Smooth Kernel by Riveeks-Smooth 
+kernel.string=Arrancar Kernel by Riveeks-Smooth 
 do.devicecheck=1
 do.modules=0
 do.systemless=1
